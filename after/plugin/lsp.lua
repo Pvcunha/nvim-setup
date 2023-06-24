3ea9e4235b1a2e9b515adb98b6e784ec1d3de799
Pedro Cunha <pvc3@cin.ufpe.br>
2023-06-23 22:55:59 -0300

    undotree and lsp

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,16 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+lsp.ensure_installed({
+  'tsserver',
+  'eslint',
+  'clangd',
+  'cmake',
+  'pyright',
+  'lua_ls'
+})
+
+lsp.setup()
--- lua/pvc/packer.lua
+++ lua/pvc/packer.lua
@@ -21,4 +21,34 @@ return require('packer').startup(function(use)
   use("christoomey/vim-tmux-navigator")
 
   use("theprimeagen/harpoon")
+
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
+
+  use('mbbill/undotree')
+
+  use {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v2.x',
+      requires = {
+        -- LSP Support
+        {'neovim/nvim-lspconfig'},             -- Required
+        {                                      -- Optional
+        'williamboman/mason.nvim',
+        run = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     -- Required
+      {'hrsh7th/cmp-nvim-lsp'}, -- Required
+      {'L3MON4D3/LuaSnip'},     -- Required
+    }
+  }
 end)
